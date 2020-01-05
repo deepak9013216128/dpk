f0059b17ede9f7a1407546955f4d54e4935d7259
deepak9013216128 <58825527+deepak9013216128@users.noreply.github.com>
2020-01-05 18:08:04 +0530

    nls command without aysc and promise

---
+++ javascript part 2!/list/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const fs = require('fs');
+fs.readdir(process.cwd(),(err,filenames)=>{
+    if(err){
+        console.log(err);
+        return err;
+    }
+    const allStats = Array(filenames.length).fill(null);
+    for(let filename of filenames){
+        const index = filenames.indexOf(filename);
+        fs.lstat(filename,(err,stats)=>{
+            if(err){
+                console.log(err)
+            }
+            allStats[index] = stats;  
+            const ready = allStats.every( stats =>( stats))
+            if(ready){
+                allStats.forEach((stats,index) => {
+                   console.log(filenames[index],stats.isFile()) 
+                });
+            }
+        })
+    }
+})
