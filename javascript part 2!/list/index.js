286486d03f81381e4c2fb65e3d51a443de74694c
deepak9013216128 <58825527+deepak9013216128@users.noreply.github.com>
2020-01-06 22:18:58 +0530

    final version and accept another directory

--- javascript part 2!/list/index.js
+++ javascript part 2!/list/index.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const chalk = require('chalk');
+const path = require('path')
 // Method #2
 // const util = require('util')
 // const lstat = util.promisify(fs.lstat)
 
 // Method #3
 const {lstat} = fs.promises;
+const targetDir = process.argv[2] || process.cwd() ;
 
-fs.readdir(process.cwd(), async (err,filenames)=>{
+fs.readdir(targetDir, async (err,filenames)=>{
     if(err){
         console.log(err);
         return err;
     }
-    const allPromises = filenames.map(filename => (lstat(filename)))
+    const allPromises = filenames.map(filename => ( lstat(path.join(targetDir,filename))) )
     const stats = await Promise.all(allPromises);
     for(let stat of stats){
         const index = stats.indexOf(stat)
